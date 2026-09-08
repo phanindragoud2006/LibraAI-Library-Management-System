18e8af1eae21e544305d4ad1936e6f139fe347ff
Pamarthi Phanindra Goud <phanindrapamarthi2006@gmail.com>
2026-09-08 18:51:26 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,1 +1,33 @@
-// Database connection class
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/library_db";
+        String username = "root";
+        String password = "Sunny@123";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    url,
+                    username,
+                    password
+            );
+
+            System.out.println("Database Connected Successfully!");
+
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Database Connection Failed!");
+            e.printStackTrace();
+
+        }
+    }
+}
